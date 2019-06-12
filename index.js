041cfb1afddee749c83c81ebe6c811e16973868d
Justin Menestrina <jmenestr@gmail.com>
2019-06-12 16:36:07 -0600

    simplify prettier config structure

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-module.exports = require('./prettier.config')
+module.exports = {
+  printWidth: 100,
+  semi: true,
+  singleQuote: true,
+  trailingComma: "es5",
+  jsxSingleQuote: true,
+  overrides: [
+
+    {
+      files: [
+        ".babelrc",
+        ".pretterrc",
+        "*.json"
+      ],
+      excludeFiles: [
+        "*.d.ts"
+      ],
+      options: {
+        parser: "json"
+      }
+    },
+
+    {
+      files: [
+        "*.ts",
+        "*.tsx"
+      ],
+      excludeFiles: [
+        "*.d.ts"
+      ],
+      options: {
+        parser: "typescript"
+      }
+    }
+  ]
+}
